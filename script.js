640ffe7a02f947364465a5e2b436558ae0ee0f53
Danusa Jesus <85651340+DanSiljer@users.noreply.github.com>
2021-11-08 00:32:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// buscar o elemento no html da minha lista onde irei inserir as vagas
-const lista = document.getElementById('lista')
 
-// atribuindo a endpoint da api do backend em um constante
+const lista = document.getElementById('lista')
 const apiUrl = 'http://localhost:3000/presidentes';
 
 // modo edicao e id edicao
 let edicao = false;
 let idEdicao = 0;
 
-// pegar os dados que o usuario digita no input (Elementos)
+// Pega os dados que o usuario digita no input (Elementos)
 let nome = document.getElementById('nome');
 let partido = document.getElementById('partido');
 let img = document.getElementById('img');
@@ -46,13 +44,9 @@ const getPresidentes= async () => {
     })
 }
 
-// [POST] envia uma presidente para o backend para ser cadastrada
-
 const submitForm = async (event) => {
-    // previnir que o navegador atualiza a pagina por causa o evento de submit
+  
     event.preventDefault();
-
-    // Estamos construindo um objeto com os valores que estamos pegando no input.
     const presidente = {
         nome: nome.value,
         partido: partido.value,
@@ -61,7 +55,7 @@ const submitForm = async (event) => {
         governo: governo.value,
         descricao: descricao.value
     }
-    // é o objeto preenchido com os valores digitados no input
+  
 
     if(edicao) {
         putPresidente(presidente, idEdicao);
@@ -74,7 +68,7 @@ const submitForm = async (event) => {
 }
 
 const createPresidente = async(presidente) => {
-    // requisicao para ser enviada para o backend.
+   
     const request = new Request(`${apiUrl}/add`, {
         method: 'POST',
         body: JSON.stringify(presidente),
@@ -83,18 +77,15 @@ const createPresidente = async(presidente) => {
         })
     })
 
-    // chamamos a funcao fetch de acordo com as nossa configuracaoes de requisicao.
+  
     const response = await fetch(request);
     const result = await response.json();
-    // pego o objeto que vem do backend e exibo a msg de sucesso em um alerta.
     alert(result.message)
-    // cadastro com sucesso.
     getPresidentes();
 
 }
 
 const putPresidentes = async(presidente, id) => {
-    // estou construindo a requisicao para ser enviada para o backend.
     const request = new Request(`${apiUrl}/edit/${id}`, {
         method:  'PUT',
         body: JSON.stringify(presidente),
@@ -103,20 +94,18 @@ const putPresidentes = async(presidente, id) => {
         })
     })
 
-    // chamamos a funcao fetch de acordo com as nossa configuracaoes de requisicao.
-    const response = await fetch(request);
 
+    const response = await fetch(request);
     const result = await response.json();
-    // pego o objeto que vem do backend e exibo a msg de sucesso em um alerta.
+   
     alert(result.message)
     edicao = false;
     idEdicao = 0;
     getPresidentes();
 }
 
-// [DELETE] funcao que exclui de acordo com o seu id
+
 const deletePresidentes = async (id) => {
-    // construir a requiscao de delete
     const request = new Request(`${apiUrl}/delete/${id}`, {
         method: 'DELETE'
     })
@@ -137,13 +126,10 @@ const getPresidenteById = async (id) => {
 }
 
 const editPresidente= async (id) => {
-    // habilitando o modo de edicao e enviando o id para variavel global de edicao.
     edicao = true;
     idEdicao = id;
 
     const presidente= await getPresidenteById(id);
-
-    //preencher os campos.
     nome.value = presidente.nome;
     partido.value =  presidente.partido;
     img.value = presidente.img;
